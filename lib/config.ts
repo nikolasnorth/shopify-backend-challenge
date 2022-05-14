a8315570ab45edc54b7457e1d29096cd785e305d
Nikolas North <nikolasn09@gmail.com>
2022-05-14 10:38:14 -0400

    add versioning to api

--- lib/config.ts
+++ lib/config.ts
@@ -1,4 +1,4 @@
 export const config = {
-  DEV_BASE_URL: "http://localhost:3000",
-  PROD_BASE_URL: "https://shopify-backend-challenge.nikolasnorth.repl.co",
+  DEV_BASE_URL: "http://localhost:3000/api/v1",
+  PROD_BASE_URL: "https://shopify-backend-challenge.nikolasnorth.repl.co/api/v1",
 };
